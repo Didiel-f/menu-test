992b24a5134e585bb7dc35483def4b4d90ba3d24
Didiel-f <didiel.f@gmail.com>
2020-08-24 12:46:33 -0400

    3 commit

---
+++ docs/precache-manifest.b18b7c235225541995fa3015f771667d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8866525e92925ee90a71727c486b73e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6526afc81ceb8d852afa",
+    "url": "/static/css/main.63617f94.chunk.css"
+  },
+  {
+    "revision": "cd99752dc3f199375d68",
+    "url": "/static/js/2.1a3adba5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1a3adba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6526afc81ceb8d852afa",
+    "url": "/static/js/main.dbee61f1.chunk.js"
+  },
+  {
+    "revision": "e1fd88f5ad02ce1354c5",
+    "url": "/static/js/runtime-main.d08f07af.js"
+  }
+]);
